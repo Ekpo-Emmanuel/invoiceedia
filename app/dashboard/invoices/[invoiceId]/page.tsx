56bab22c1077836a6d765f8de3882890685fd32c
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-17 16:43:09 -0800

    feat(invoice): implement invoice details page with user authentication
    - Fetch invoice details based on `invoiceId` and `userId`.
    - Add user authentication via `Clerk` to ensure only authorized users can access their invoices.
    - Integrate `notFound` to handle cases where the invoice is not found.
    - Display invoice details using the `Invoice` component.

--- app/dashboard/invoices/[invoiceId]/page.tsx
+++ app/dashboard/invoices/[invoiceId]/page.tsx
@@ -1,23 +1,38 @@
-import React from 'react'
 
-import { db } from "@/db"
-import { Invoices } from '@/db/schema'
-import { eq } from 'drizzle-orm';
+import React from "react";
+import { auth } from "@clerk/nextjs/server";
+
+import { db } from "@/db";
+import { Invoices } from "@/db/schema";
+import { and, eq } from "drizzle-orm";
+import { Badge } from "@/components/ui/badge";
+import { notFound } from "next/navigation";
+import Invoice from "./invoice";
 
 
-export default async function InvoicePage({ params }: { params: Promise<{ invoiceId: string }> }) {
-  const invoiceId  = (await params).invoiceId;
+export default async function InvoicePage({
+  params,
+}: {
+  params: Promise<{ invoiceId: string }>;
+}) {
+  const { userId } = await auth();
+  if (!userId) return;
+
+  const invoiceId = (await params).invoiceId;
   const invoiceIdNumber = parseInt(invoiceId);
 
-  const [result] = await db.select()
+  // if(isNaN(invoiceId)) {
+  //   throw new Error('Invalid Invoice ID')
+  // }
+  const [result] = await db
+    .select()
     .from(Invoices)
-    .where(eq(Invoices.id, invoiceIdNumber));
+    .where(and(eq(Invoices.id, invoiceIdNumber), eq(Invoices.userId, userId)));
 
+  if (!result) {
+    notFound();
+  }
   return (
-    <div className="p-4 max-w-7xl mx-auto">
-      <div className='flex items-center justify-between'>
-        <h2 className='text-3xl font-semibold'>Invoices {invoiceIdNumber}</h2>
-      </div>
-    </div>
-  )
+    <Invoice invoice={result}/>
+  );
 }
