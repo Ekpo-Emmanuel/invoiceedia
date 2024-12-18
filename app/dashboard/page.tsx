2adaa310d47114699b7f335973b28df4626475b3
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-17 16:40:50 -0800

    chore(home): integrate Clerk authentication and update UI buttons
    - Add Clerk authentication handling with `SignInButton` and `UserButton`.
    - Update UI to show relevant buttons based on user authentication state.
    - Maintain "Get started" button for dashboard navigation.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,93 +1,120 @@
-import React from 'react'
+import React from "react";
+import { auth } from "@clerk/nextjs/server";
 
-import { db } from "@/db"
-import { Invoices } from '@/db/schema'
+
+import { db } from "@/db";
+import { Invoices } from "@/db/schema";
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
-import { Badge } from "@/components/ui/badge"
-import { Button } from '@/components/ui/button'
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { CirclePlus } from 'lucide-react';
+import { CirclePlus } from "lucide-react";
+import clsx from 'clsx';
+import { eq } from "drizzle-orm";
 
-  
 
 export default async function page() {
-    const results = await db.select().from(Invoices);
+  const { userId } = await auth();
+  if (!userId) return;
+
+  const results = await db.select()
+    .from(Invoices)
+    .where(eq(Invoices.userId, userId));
+
   return (
-    <div className="p-4 max-w-7xl mx-auto">
-        <div className='flex items-center justify-between'>
-            <h2 className='text-3xl font-semibold'>Invoices</h2>
-            <Button asChild >
-                <Link href="/dashboard/invoices/new" className='flex items-center gap-2'>
-                    <CirclePlus className='w-4 h-4'  /> 
-                    Add Invoice
-                </Link>
-            </Button>
-        </div>
-      <Table className='mt-4'>
+    <div className="p-4 max-w-7xl mx-auto w-full">
+      <div className="flex items-center justify-between">
+        <h2 className="text-3xl font-semibold">Invoices</h2>
+        <Button asChild>
+          <Link
+            href="/dashboard/invoices/new"
+            className="flex items-center gap-2"
+          >
+            <CirclePlus className="w-4 h-4" />
+            Add Invoice
+          </Link>
+        </Button>
+      </div>
+      <Table className="mt-4">
         <TableCaption>A list of your recent invoices.</TableCaption>
         {/* Table Header */}
         <TableHeader>
-            <TableRow>
-            <TableHead className="w-[100px]">
-                Date
-            </TableHead>
-            <TableHead className=''>
-                Customer
-            </TableHead>
-            <TableHead>
-                Email
-            </TableHead>
-            <TableHead className="w-[100px]">
-                Status
-            </TableHead>
-            <TableHead className="text-right">
-                Value
-            </TableHead>
-            </TableRow>
+          <TableRow>
+            <TableHead className="w-[100px]">Date</TableHead>
+            <TableHead className="">Customer</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead className="w-[100px]">Status</TableHead>
+            <TableHead className="text-right">Value</TableHead>
+          </TableRow>
         </TableHeader>
         <TableBody>
-            {/* Single Table Row */}
-            {results.map((invoice) => (
-                <TableRow key={invoice.id}>
-                    <TableCell className="p-0">
-                        <Link href={`/dashboard/invoices/${invoice.id}`} className='font-semibold py-4 block'>
-                            {new Date(invoice.createTs).toLocaleDateString()}
-                        </Link>
-                    </TableCell>
-                    <TableCell className="p-0">
-                        <Link href={`/dashboard/invoices/${invoice.id}`} className='font-semibold py-4 block'>
-                            {/* {invoice.name} */}
-                            John Doe
-                        </Link>
-                    </TableCell>
-                    <TableCell className='p-0'>
-                        <Link href={`/dashboard/invoices/${invoice.id}`} className='py-4 block'>
-                            {/* {invoice.email} */}
-                            johndoe.email.com
-                        </Link>
-                    </TableCell>
-                    <TableCell className="p-0">
-                        <Link href={`/dashboard/invoices/${invoice.id}`} className='py-4 block'>
-                            <Badge className='rounded-full'>{invoice.status}</Badge>
-                        </Link>
-                    </TableCell>
-                    <TableCell className="text-right p-0">
-                        <Link href={`/dashboard/invoices/${invoice.id}`} className='font-semibold'>
-                            ${(invoice.value / 100).toFixed(2)}
-                        </Link>
-                    </TableCell>
-                </TableRow>
-            ))}
+          {/* Single Table Row */}
+          {results.map((invoice) => (
+            <TableRow key={invoice.id}>
+              <TableCell className="p-0">
+                <Link
+                  href={`/dashboard/invoices/${invoice.id}`}
+                  className="font-semibold py-4 block"
+                >
+                  {new Date(invoice.createTs).toLocaleDateString()}
+                </Link>
+              </TableCell>
+              <TableCell className="p-0">
+                <Link
+                  href={`/dashboard/invoices/${invoice.id}`}
+                  className="font-semibold py-4 block"
+                >
+                  {/* {invoice.name} */}
+                  John Doe
+                </Link>
+              </TableCell>
+              <TableCell className="p-0">
+                <Link
+                  href={`/dashboard/invoices/${invoice.id}`}
+                  className="py-4 block"
+                >
+                  {/* {invoice.email} */}
+                  johndoe.email.com
+                </Link>
+              </TableCell>
+              <TableCell className="p-0">
+                <Link
+                  href={`/dashboard/invoices/${invoice.id}`}
+                  className="py-4 block"
+                >
+                  <Badge
+                    className={clsx(
+                      "rounded-full h-fit capitalize",
+                      invoice.status === "open" && "bg-blue-500",
+                      invoice.status === "paid" && "bg-green-500",
+                      invoice.status === "void" && "bg-zinc-700",
+                      invoice.status === "uncollectible" && "bg-red-600"
+                    )}
+                  >
+                    {invoice.status}
+                  </Badge>
+                </Link>
+              </TableCell>
+              <TableCell className="text-right p-0">
+                <Link
+                  href={`/dashboard/invoices/${invoice.id}`}
+                  className="font-semibold"
+                >
+                  ${(invoice.value / 100).toFixed(2)}
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
-        </Table>
+      </Table>
     </div>
-  )
+  );
 }
