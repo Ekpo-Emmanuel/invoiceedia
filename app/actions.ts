4babb2f94ebbe463feb8077a71419259acdd2f34
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-17 16:37:15 -0800

    feat(invoices): add dynamic status enum and update table schema
    - Generate `StatusEnum` dynamically from `AVAILABLE_STATUSES` for consistency.
    - Add `userId` column to the `invoices` table.
    - Ensure alignment of backend enums with frontend definitions.

--- app/actions.ts
+++ app/actions.ts
@@ -1,11 +1,18 @@
 "use server";
 
 import { db } from "@/db";
-import { Invoices } from "@/db/schema";
+import { Invoices, Status } from "@/db/schema";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-
+import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
 
 export async function createAction(formData: FormData) {
+    const { userId } = await auth();
+    if (!userId) {
+        throw new Error("User not authenticated");
+    }
+    
     const rawValue = formData.get("value") as string;
     if (!rawValue || isNaN(parseFloat(rawValue))) {
         throw new Error("Invalid value provided");
@@ -16,6 +23,7 @@ export async function createAction(formData: FormData) {
     const results = await db.insert(Invoices).values({
         value,
         description,
+        userId,
         status: 'open'
     })
     .returning({
@@ -23,4 +31,24 @@ export async function createAction(formData: FormData) {
     })
 
     redirect(`/dashboard/invoices/${results[0].id}`);
+}
+
+export async function updateStatusAction(formData: FormData) {
+    const { userId } = await auth();
+    if (!userId) {
+        throw new Error("User not authenticated");
+    }
+
+    const id = formData.get('id') as string;
+    const status = formData.get('status') as Status;
+
+    const results = await db.update(Invoices)
+        .set({ status })
+        .where(
+            and(
+                eq(Invoices.id, parseInt(id)),
+                eq(Invoices.userId, userId)
+            ))
+
+    revalidatePath(`/invoices/${id}`, 'page');
 }
