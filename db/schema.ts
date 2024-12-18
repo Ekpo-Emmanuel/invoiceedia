bf5c595673d5238d8e708b7d24bca2365943cf93
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-17 16:36:42 -0800

    feat(invoices): add dynamic status enum and update table schema
    - Generate `StatusEnum` dynamically from `AVAILABLE_STATUSES` for consistency.
    - Add `userId` column to the `invoices` table.
    - Ensure alignment of backend enums with frontend definitions.

--- db/schema.ts
+++ db/schema.ts
@@ -1,12 +1,17 @@
 import { integer, pgEnum, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { AVAILABLE_STATUSES } from '@/app/dashboard/(data)/invoices';
 
+export type Status = typeof AVAILABLE_STATUSES[number]["id"];
 
-export const StatusEnum = pgEnum('status', ['open', 'canceled', 'uncollectible', 'pending', 'paid', 'failed'])
+const statuses = AVAILABLE_STATUSES.map(({id}) => id) as Array<Status>;
+
+export const StatusEnum = pgEnum('status', statuses as [Status, ...Array<Status>]);
 
 export const Invoices = pgTable('invoices', {
     id: serial('id').primaryKey().notNull(),
     createTs: timestamp('create_ts').defaultNow().notNull(),
     value: integer('value').notNull(),
     description: text('description').notNull(),
+    userId: text('userId').notNull(),
     status: text('status').notNull(),
 });
