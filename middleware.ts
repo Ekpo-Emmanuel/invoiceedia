6ae7f86ae370da50cb9f4361cd486050058c5d2a
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-18 23:12:38 -0800

    fix(middleware): adjust public route matcher and add payment path
    - Updated `isPublicRoute` to include `/dashboard/invoices/(.*)/payment` as a public route.
    - Ensured non-public routes are protected using Clerk's `auth.protect()`.
    - Preserved configuration for matching API routes and skipping Next.js internals and static files.

--- middleware.ts
+++ middleware.ts
@@ -4,15 +4,9 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
     '/',
     '/sign-in(.*)', 
-    '/sign-up(.*)'
+    '/sign-up(.*)',
+    '/dashboard/invoices/(.*)/payment'
 ])
-
-// const isProtectedRoute = createRouteMatcher([
-//     '/dashboard',
-//     '/invoices/:invoiceId',
-//     '/invoices/new'
-// ])
-
 
 export default clerkMiddleware(async (auth, req) => {
     if (!isPublicRoute(req)) {
