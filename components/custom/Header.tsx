33820106918cd268ad80fb31e696090857ca8541
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-18 23:18:26 -0800

    chore: Add payment handling and invoice management UI
    - Implemented payment status handling with Stripe for invoice payment confirmation
    - Added UI for managing invoice payment and displaying status messages (success, failure, canceled)
    - Included conditional rendering for different invoice statuses (open, paid)
    - Enhanced invoice details section with customer name, invoice ID, and date
    - Integrated error handling for payment failure and cancellation

--- components/custom/Header.tsx
+++ components/custom/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import {
+    OrganizationSwitcher,
   SignInButton,
   SignedIn,
   SignedOut,
@@ -12,9 +13,21 @@ export default function Header() {
   return (
         <header className="max-w-7xl w-full mx-auto px-4 mt-6 mb-12">
             <div className="flex items-center justify-between">  
-                <p className="font-bold">
-                    <Link href="/">Invoicedia</Link>
-                </p>
+                <div className="flex items-center gap-4">
+                    <p className="font-bold">
+                        <Link href="/dashboard">Invoicedia</Link>
+                    </p>
+                    <SignedIn>
+                        <span className="text-slate-300">/</span>
+                    </SignedIn>
+                    <SignedIn>
+                        <div className="">
+                            <OrganizationSwitcher 
+                                afterCreateOrganizationUrl={"/dashboard"}
+                            />
+                        </div>
+                    </SignedIn>
+                </div>
                 <div>
                     <SignedOut>
                     <Button asChild variant={"outline"}>
