5b7d68973c62bf3574dd48c78a294912e1c08156
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-19 14:29:31 -0800

    refactor: replace home page content with LandingPage component
    - Replaced inline content of the Home component with the `LandingPage` component for better modularity and structure.
    - Commented out the previous JSX in favor of using the `LandingPage` component to display the landing page.
    - Simplified the Home component for easier maintenance and extensibility.

--- app/page.tsx
+++ app/page.tsx
@@ -9,26 +9,29 @@ import {
   SignedOut,
   UserButton
 } from '@clerk/nextjs'
+import LandingPage from "@/components/custom/Landing page/Container";
 
 export default async function Home() {
-  const result = await db.execute(sql`SELECT current_database()`);
-
-  // console.log(result);
   return (
-    <div className="h-screen w-full flex flex-col items-center justify-center">
-      <div className="flex gap-2">
-        <Button asChild>
-          <Link href="/dashboard">Get started</Link>
-        </Button>
-        <SignedOut>
-          <Button asChild variant={"outline"}>
-            <SignInButton />
+    <>
+      {/* <div className="h-screen w-full flex flex-col items-center justify-center">
+        <div className="flex gap-2">
+          <Button asChild>
+            <Link href="/dashboard">Get started</Link>
           </Button>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+          <SignedOut>
+            <Button asChild variant={"outline"}>
+              <SignInButton />
+            </Button>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+        </div>
+      </div> */}
+      <div className="">
+        <LandingPage />
       </div>
-    </div>
+    </>
   );
 }
