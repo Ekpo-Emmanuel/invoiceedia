656e1c22ab3304157bafaceea5d574ada34983c5
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-18 23:20:16 -0800

    feat: Modify Invoices schema and add Customers schema
    - Updated the Invoices schema to include `organizationId`, `customerId`, and reference the `Customers` table.
    - Replaced dynamic `StatusEnum` with a predefined set of invoice statuses.
    - Introduced a new `Customers` table with fields for `name`, `email`, and relational data with `userId` and `organizationId`.

--- db/schema.ts
+++ db/schema.ts
@@ -4,8 +4,9 @@ import { AVAILABLE_STATUSES } from '@/app/dashboard/(data)/invoices';
 export type Status = typeof AVAILABLE_STATUSES[number]["id"];
 
 const statuses = AVAILABLE_STATUSES.map(({id}) => id) as Array<Status>;
+export const StatusEnum = pgEnum('status', ['open', 'paid', 'void', 'uncollectible', 'canceled', 'pending', 'failed']);
 
-export const StatusEnum = pgEnum('status', statuses as [Status, ...Array<Status>]);
+// export const StatusEnum = pgEnum('status', statuses as [Status, ...Array<Status>]);
 
 export const Invoices = pgTable('invoices', {
     id: serial('id').primaryKey().notNull(),
@@ -13,5 +14,16 @@ export const Invoices = pgTable('invoices', {
     value: integer('value').notNull(),
     description: text('description').notNull(),
     userId: text('userId').notNull(),
-    status: text('status').notNull(),
+    organizationId: text('organizationId'),
+    customerId: integer('customerId').notNull().references(() => Customers.id),
+    status: StatusEnum('status').notNull(),
+});
+
+export const Customers = pgTable('customers', {
+    id: serial('id').primaryKey().notNull(),
+    createTs: timestamp('create_ts').defaultNow().notNull(),
+    name: text('name').notNull(),
+    email: text('email').notNull(),
+    userId: text('userId').notNull(),
+    organizationId: text('organizationId'),
 });
