40743056423e5476ef8cdab550ab4bacd6a2e081
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-12-24 07:07:59 -0800

    refactor: Remove commented-out code in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,8 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button"
-import {sql} from "drizzle-orm"
-import {db} from "@/db"
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
 import LandingPage from "@/components/custom/Landing page/Container";
 
 export default async function Home() {
   return (
     <>
-      {/* <div className="h-screen w-full flex flex-col items-center justify-center">
-        <div className="flex gap-2">
-          <Button asChild>
-            <Link href="/dashboard">Get started</Link>
-          </Button>
-          <SignedOut>
-            <Button asChild variant={"outline"}>
-              <SignInButton />
-            </Button>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-        </div>
-      </div> */}
       <div className="">
         <LandingPage />
       </div>
