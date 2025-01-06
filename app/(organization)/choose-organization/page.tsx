903959bd2f3a1b41c4982e90ae72cd09655f1cb9
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2025-01-06 11:58:41 -0800

    feat: Add ChooseOrganization page with header component

---
+++ app/(organization)/choose-organization/page.tsx
@@ -0,0 +1,12 @@
+import OrganizationHeader from "@/components/v2/headers/organiztion-header";
+import ChooseOrganization from "@/components/v2/organization/choose-organization";
+
+export default function page() {
+
+  return (
+    <div>
+      <OrganizationHeader />
+      <ChooseOrganization />
+    </div>
+  );
+}
