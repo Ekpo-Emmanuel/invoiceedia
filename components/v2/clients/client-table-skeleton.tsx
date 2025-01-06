e663bf467dab0e1368fe719747dfb20f694e9c49
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2025-01-06 14:32:52 -0800

    feat: Add skeleton component for client table loading state

---
+++ components/v2/clients/client-table-skeleton.tsx
@@ -0,0 +1,36 @@
+import { Skeleton } from "@/components/ui/skeleton"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+export function ClientTableSkeleton() {
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead className="hidden md:table-cell">Company</TableHead>
+            <TableHead className="hidden md:table-cell">Location</TableHead>
+            <TableHead className="text-right">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Array.from({ length: 10 }).map((_, i) => (
+            <TableRow key={i}>
+              <TableCell colSpan={9}>
+                <Skeleton className="h-5 w-full" />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  )
+} 
