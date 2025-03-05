ba66babcf3da0fa55622b77c7e6b70baf6b0370a
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2025-03-05 08:11:10 -0800

    feat: Implement advanced invoice filtering with Popover and AdvancedFilters component

--- components/v2/invoices/recent-invoices/status-filter.tsx
+++ components/v2/invoices/recent-invoices/status-filter.tsx
@@ -9,27 +9,44 @@ import {
   TooltipTrigger,
   TooltipProvider
 } from "@/components/ui/tooltip";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import AdvancedFilters, { FilterValues } from '../advanced-filters';
+
 
 interface StatusFilterProps {
-  ontoggleAdvancedFilters: () => void;
+  clients: Array<{ id: string; name: string }>;
+  onFilterChange: (filters: FilterValues) => void;
 }
 
-export default function StatusFilter({ ontoggleAdvancedFilters }: StatusFilterProps) {
+export default function StatusFilter({ clients, onFilterChange }: StatusFilterProps) {
   return (
-      <TooltipProvider disableHoverableContent>
-        <Tooltip delayDuration={100}>
-          <TooltipTrigger asChild>
-              <Button
-                variant="outline"
-                size="sm"
-                className=""
-                onClick={ontoggleAdvancedFilters}
-              >
-                <SlidersHorizontal className="w-4 h-4" />
-              </Button>
-          </TooltipTrigger>
-          <TooltipContent side="bottom">filter</TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+    <Popover>
+      <PopoverTrigger>
+        <TooltipProvider disableHoverableContent>
+          <Tooltip delayDuration={100}>
+            <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className=""
+                >
+                  <SlidersHorizontal className="w-4 h-4" />
+                </Button>
+            </TooltipTrigger>
+            <TooltipContent side="bottom">filter</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </PopoverTrigger>
+      <PopoverContent className="w-[400px] -translate-x-10 dark:bg-[#1b1b1d] border dark:border-primary/10 rounded-lg">
+        <AdvancedFilters 
+          clients={clients}
+          onFilterChange={onFilterChange}
+        />
+      </PopoverContent>
+    </Popover>
   );
 }
