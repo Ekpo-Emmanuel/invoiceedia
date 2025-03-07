223c35897175800599ca8c5f72444e0b5188a2b1
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2025-03-07 00:10:55 -0800

    refactor: Simplify middleware logic and enhance public route matching

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,21 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
-    '/',
-    '/sign-in(.*)', 
-    '/sign-up(.*)',
-    '/forgot-password(.*)',
-    '/verify-email(.*)',  
-    '/sso-callback(.*)',  
-    '/dashboard/invoices/(.*)/payment'
-])
+  '/',
+  '/sign-in(.*)', 
+  '/sign-up(.*)',
+  '/sso-callback(.*)',
+  '/api/webhooks(.*)'
+]);
 
-export default clerkMiddleware(async (auth, req) => {
-    if (!isPublicRoute(req)) {
-        await auth.protect()
-    }
-  })
-  
-   
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth.protect();
+  }
+  return NextResponse.next();
+});
+
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
